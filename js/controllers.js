bd0506894db8edf7a14f63969bd2cae2a9149310
Klaudiusz Dembler <k.dembler@gmail.com>
2015-11-04 00:10:31 +0100

    finished login, started angular migration

---
+++ js/controllers.js
@@ -0,0 +1,8 @@
+/**
+* scriptumApp Module
+*
+* Description
+*/
+angular.module('scriptumApp', ['duScroll']).controller('bodyCtrl', function($scope, $document){
+
+})
--- js/main.js
+++ js/main.js
@@ -2,19 +2,26 @@
   $(function() {
 
     $('.parallax').parallax();
-    $('#getstarted-button').click(function(e) {
-      e.preventDefault();
-      $('html,body').animate({
-        scrollTop: $("#content").offset().top
-      }, 'slow');
-    });
     $('.modal-trigger').leanModal();
 
   }); // end of document ready
+  function scrollToId(id) {
+    $('html,body').animate({
+      scrollTop: $(id).offset().top
+    }, 'slow');
+  };
+
+  $('#getstarted-button').click(function(e) {
+    e.preventDefault();
+    scrollToId("#content");
+  });$('#scrolltop-button').click(function(e) {
+    e.preventDefault();
+    scrollToId("#header");
+  });
   $(".hover-lock").hover(function() {
     $(this).find("i").html("lock_open");
   }, function() {
     $(this).find("i").html("lock_outline");
-  })(jQuery); // end of jQuery name space
+  }); // end of jQuery name space
 
 })(jQuery);
