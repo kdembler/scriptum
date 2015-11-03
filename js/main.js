7a1d36df6870aa04ee8e2ebbe25ee4d45a85d315
Klaudiusz Dembler <k.dembler@gmail.com>
2015-11-03 20:45:26 +0100

    Login modal work

--- js/main.js
+++ js/main.js
@@ -7,7 +7,14 @@
       $('html,body').animate({
         scrollTop: $("#content").offset().top
       }, 'slow');
-    })
+    });
+    $('.modal-trigger').leanModal();
 
   }); // end of document ready
-})(jQuery); // end of jQuery name space
+  $(".hover-lock").hover(function() {
+    $(this).find("i").html("lock_open");
+  }, function() {
+    $(this).find("i").html("lock_outline");
+  })(jQuery); // end of jQuery name space
+
+})(jQuery);
