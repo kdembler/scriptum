ac1165a8389d9c29b92eac5f91795039584f506e
Klaudiusz Dembler <k.dembler@gmail.com>
2016-02-02 12:49:03 +0100

    finished controllers

--- public/javascripts/scriptumApp/controllers/homeCtrl.js
+++ public/javascripts/scriptumApp/controllers/homeCtrl.js
@@ -1,5 +1,27 @@
 var HomeCtrl = function($scope, posts, auth) {
+    $scope.posts = posts.posts;
 
+    $scope.addPost = function() {
+        if(!$scope.title || $scope.title === '') return;
+        if(!scope.body || $scope.body === '') return;
+
+        posts.create({
+            title: $scope.title,
+            body: $scope.body
+        });
+        $scope.title = '';
+        $scope.body = '';
+    };
+
+    $scope.likePost = function(post) {
+        post.like(post);
+    };
+
+    $scope.dislikePost = function(post) {
+        post.dislike(post);
+    };
+
+    $scope.isLoggedIn = auth.isLoggedIn;
 };
 
 HomeCtrl.$inject = ['$scope', 'posts', 'auth'];
--- public/javascripts/scriptumApp/controllers/postCtrl.js
+++ public/javascripts/scriptumApp/controllers/postCtrl.js
@@ -1,5 +1,22 @@
 var PostCtrl = function($scope, posts, post, auth) {
+    $scope.post = post;
 
+    $scope.addComment = function() {
+        if(!$scope.body || $scope.body === '') return;
+
+        posts.addComment(post, {
+            body: $scope.body
+        }).success(function(comment) {
+            $scope.post.comments.push(comment);
+        });
+        $scope.body = '';
+    };
+
+    $scope.likeComment = function(comment) {
+        posts.likeComment(post, comment);
+    };
+
+    $scope.isLoggedIn = auth.isLoggedIn;
 };
 
 PostCtrl.$inject = ['$scope', 'posts', 'post', 'auth'];
