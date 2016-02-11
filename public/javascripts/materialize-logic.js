10f5f68514ecc75cc5407409c265bdc4f495643a
Klaudiusz Dembler <k.dembler@gmail.com>
2016-02-11 21:47:32 +0100

    tweaked submit button

--- public/javascripts/materialize-logic.js
+++ public/javascripts/materialize-logic.js
@@ -2,4 +2,43 @@ $(document).ready(function() {
     $('.parallax').parallax();
     $('.modal-trigger').leanModal();
     $('ul.tabs').tabs();
+    // override materialize card reveal behaviour
+    $(document).off('click.card', '.card').on('click.card', '.card', function(e) {
+        if ($(this).find('> .card-reveal').length) {
+            if ($(e.target).is($('.card-reveal .card-title')) || $(e.target).is($('.card-reveal .card-title i'))) {
+                $(e.target).closest('.card').velocity('reverse');
+                $(this).find('.btn-scriptum-submit').css({opacity: '0'});
+                $(this).find('.card-reveal').velocity({
+                    translateY: 0
+                }, {
+                    duration: 225,
+                    queue: false,
+                    easing: 'easeInOutQuad',
+                    complete: function() {
+                        $(this).css({
+                            display: 'none'
+                        });
+                    }
+                });
+            } else if ($(e.target).is($('.card .activator')) ||
+                $(e.target).is($('.card .activator i'))) {
+                $(e.target).closest('.card').css('overflow', 'hidden');
+                $(this).find('.btn-scriptum-submit').css({opacity: '1'});
+                $(e.target).closest('.card').velocity({
+                    height: 500
+                }, 225, 'easeInOutQuad', function() {});
+                $(this).find('.card-reveal').css({
+                    display: 'block'
+                }).velocity("stop", false).velocity({
+                    translateY: '-100%'
+                }, {
+                    duration: 300,
+                    queue: false,
+                    easing: 'easeInOutQuad'
+                });
+            }
+        }
+
+        $('.card-reveal').closest('.card').css('overflow', 'hidden');
+    });
 });
