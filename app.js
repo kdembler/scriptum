5d967f9720fb9787e49db7fc934aa1a41674bec0
Klaudiusz Dembler <k.dembler@gmail.com>
2018-03-12 17:06:34 +0100

    Add favicon

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(favicon(path.join(__dirname, 'public', 'fav', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
