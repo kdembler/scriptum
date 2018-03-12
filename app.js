b5ba52bf4c09b42db85a447f37e23196935986c5
Klaudiusz Dembler <k.dembler@gmail.com>
2018-03-12 16:21:29 +0100

    Remove bin/www

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var debug = require('debug')('scriptum:server');
+var http = require('http');
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -66,5 +69,58 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
 
 mongoose.connect('mongodb://localhost/scriptum');
